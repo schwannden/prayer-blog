3d63c4f3eb8b2bc3939c2b63611c15bd108ab4bc
schwannden <schwannden@gmail.com>
2024-07-24 21:08:06 +0800

    feat(prayer): adding luke's prayer (#6)

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,6 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: '靈命塑造的禱告：路加福音的五個禱告',
+    description: '路加福音中圍繞著耶穌誕生的五個禱告，幫助我們塑造一個全天禱告的節奏。',
+    imgSrc: '/static/images/projects/day-of-prayer.png',
+    href: '/prayer/luke-prayer/intro',
+  },
+  {
     title: '為孩子禱告',
     description: `為你的孩子禱告`,
     imgSrc: '/static/images/projects/children.png',
