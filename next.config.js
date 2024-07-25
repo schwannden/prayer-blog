6f06022cb881dff0a330ad417251e867d1b2f005
schwannden <schwannden@gmail.com>
2024-07-25 13:52:53 +0800

    feat(config): add analytics (#8)

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
-  connect-src *;
+  connect-src * www.google-analytics.com www.googletagmanager.com;
   font-src 'self';
   frame-src giscus.app
 `
