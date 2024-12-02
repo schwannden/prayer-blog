5f717a66662144980ff9267b3ae6f930d5a0a5d7
Schwannden Kuo <schwannden@gmail.com>
2024-12-02 21:22:12 +0800

    fix: next 15 async api
    fix by npx @next/codemod@canary next-async-request-api .

--- app/prayer/[...slug]/page.tsx
+++ app/prayer/[...slug]/page.tsx
@@ -21,11 +21,12 @@ const layouts = {
   PostBanner,
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string[] }
-}): Promise<Metadata | undefined> {
+export async function generateMetadata(
+  props: {
+    params: Promise<{ slug: string[] }>
+  }
+): Promise<Metadata | undefined> {
+  const params = await props.params;
   const slug = decodeURI(params.slug.join('/'))
   const post = allPrayers.find((p) => p.slug === slug)
   const authorList = post?.authors || ['default']
@@ -78,7 +79,8 @@ export const generateStaticParams = async () => {
   return allPrayers.map((p) => ({ slug: p.slug.split('/').map((name) => decodeURI(name)) }))
 }
 
-export default async function Page({ params }: { params: { slug: string[] } }) {
+export default async function Page(props: { params: Promise<{ slug: string[] }> }) {
+  const params = await props.params;
   const slug = decodeURI(params.slug.join('/'))
   // Filter out drafts in production
   const sortedCoreContents = allCoreContent(sortPosts(allPrayers))
